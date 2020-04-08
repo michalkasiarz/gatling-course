f198d03e69cb1f82f4fffc6af55454bcb4981a11
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-08 16:58:00 +0200

    ramp users per sec load simulation

---
+++ project/RampUsersPerSecLoadSimulation.scala
@@ -0,0 +1,42 @@
+package simulations
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration._
+
+class RampUsersPerSecLoadSimulation extends Simulation {
+
+  val httpConf = http.baseUrl("http://localhost:8080/app/")
+    .header("Accept", "application/json")
+
+  def getAllVideoGames() = {
+    exec(
+      http("Get all video games")
+        .get("videogames")
+        .check(status.is(200))
+    )
+  }
+
+  def getSpecificGame() = {
+    exec(
+      http("Get specific game")
+        .get("videogames/2")
+        .check(status.is(200))
+    )
+  }
+
+  val scn = scenario("Basic Load Simulation")
+    .exec(getAllVideoGames())
+    .pause(5)
+    .exec(getSpecificGame())
+    .pause(5)
+    .exec(getAllVideoGames())
+
+  setUp(
+    scn.inject(
+      nothingFor(5 seconds),
+      rampUsersPerSec(1) to (5) during (20 seconds))
+  ).protocols(httpConf.inferHtmlResources()
+  )
+}
